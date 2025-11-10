4a0868651b0a98386d0a5391c3f10c60476f0899
mixplusads-ar <mixplus.ads.ar@gmail.com>
2025-11-10 18:46:53 +0330

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  integrations: [tailwind()],
-  site: 'https://mixplus.co', // change to the real domain later
-  output: 'static'
+  output: 'static',
+  integrations: [tailwind({ applyBaseStyles: false })],
 });
