0b546f18dac5d8c0516107bfd77762edacab03c9
mixplusads-ar <mixplus.ads.ar@gmail.com>
2025-11-11 13:50:43 +0330

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,9 @@
+/* Mini reset (چون Tailwind Preflight خاموش است) */
+* { box-sizing: border-box; }
+html, body { margin: 0; padding: 0; }
+ul, ol { list-style: none; margin: 0; padding: 0; }
+a { text-decoration: none; color: inherit; }
+
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
